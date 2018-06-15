a54b2070b1124d35fb96e9b7453275a307e70c5d
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-15 15:43:41 -0300

    RBF finalida

--- RBF.m
+++ RBF.m
@@ -9,27 +9,27 @@ clc
 
 load iris_log.dat; %Carrega a base de dados
 
-base = iris_log;
-[tam, ~] = size(iris_log);
-x = base(:,1:4)';
-y = base(:,5:7)';
+base = iris_log; %base recebe a base de dados
+[tam, ~] = size(iris_log); %tam recebe o numero de elementos
+x = base(:,1:4)'; %x recebe todos os atributos
+y = base(:,5:7)'; %y recebe todas as classes
 
-tamTreino = ceil(0.8*tam);
-for i=1:50
-    [X,Y] = permuta(x,y);    
-    net = newrb(X(:,1:tamTreino),Y(:,1:tamTreino));
-    Yout = sim(net,X(:,tamTreino+1:tam));
-    acertos = 0;
-    for j=tamTreino+1:tam
-        [~,indice1] = max(Yout(:,j-tamTreino));
-        [~,indice2] = max(Y(:,j));
-        if indice1 == indice2
-            acertos = acertos + 1;
+tamTreino = ceil(0.8*tam); %tamTreino calcula os 80% do treino
+for i=1:50 %de 1 ate 50
+    [X,Y] = permuta(x,y); %X recebe x permutado e Y recebe y permutado
+    net = newrb(X(:,1:tamTreino),Y(:,1:tamTreino)); %criamos uma RBF com os conjuntos treino X e Y
+    Yout = sim(net,X(:,tamTreino+1:tam)); %obtemos a saida Yout estimada para os Y do teste
+    acertos = 0; %Cada rede inicia com acerto 0
+    for j=tamTreino+1:tam %j percorre os dados de teste
+        [~,indice1] = max(Yout(:,j-tamTreino)); %indice1 recebe o indice onde ocorre o maximo de Yout(:,j)
+        [~,indice2] = max(Y(:,j)); %indice2 recebe o indice onde ocorre o maximo de Y(:,j)
+        if indice1 == indice2 %se indice1 == indice2
+            acertos = acertos + 1; %acrescenta acertos
         end
     end
-    vetAcertos(i) = acertos;
+    vetAcertos(i) = acertos; %vetAcertos(i) recebe o numero de acertos da i-esima rede
 end
-fprintf('\nResultados obtidos:\n')
+fprintf('\nResultados obtidos:\n'); %exibe a mensagem
 fprintf('Porcentagem media de acerto: %.4f\n',mean(vetAcertos)*100/(tam-tamTreino)); %exibe a taxa media de acerto
 fprintf('Percenteagem minima de acerto: %.4f\n',min(vetAcertos)*100/(tam-tamTreino)); %exibe a menor taxa de acerto
 fprintf('Percentagem maxima de acerto: %.4f\n',max(vetAcertos)*100/(tam-tamTreino)); %exibe a maior taxa de acerto
