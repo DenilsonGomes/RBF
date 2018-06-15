7b68346db63b65589b4cb07efd94bcfd6ad8cfa1
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-15 15:20:05 -0300

    Codigo Finalizado

--- RBF.m
+++ RBF.m
@@ -4,6 +4,9 @@
 %Programa para calcular a taxa de acerto da RBF na classificação da base
 %iris_log
 
+clear
+clc
+
 load iris_log.dat; %Carrega a base de dados
 
 base = iris_log;
@@ -15,7 +18,7 @@ tamTreino = ceil(0.8*tam);
 for i=1:50
     [X,Y] = permuta(x,y);    
     net = newrb(X(:,1:tamTreino),Y(:,1:tamTreino));
-    Yout = sim(net,X(:,tamTreino:tam));
+    Yout = sim(net,X(:,tamTreino+1:tam));
     acertos = 0;
     for j=tamTreino+1:tam
         [~,indice1] = max(Yout(:,j-tamTreino));
@@ -26,3 +29,7 @@ for i=1:50
     end
     vetAcertos(i) = acertos;
 end
+fprintf('\nResultados obtidos:\n')
+fprintf('Porcentagem media de acerto: %.4f\n',mean(vetAcertos)*100/(tam-tamTreino)); %exibe a taxa media de acerto
+fprintf('Percenteagem minima de acerto: %.4f\n',min(vetAcertos)*100/(tam-tamTreino)); %exibe a menor taxa de acerto
+fprintf('Percentagem maxima de acerto: %.4f\n',max(vetAcertos)*100/(tam-tamTreino)); %exibe a maior taxa de acerto
